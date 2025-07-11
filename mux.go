13fae9226a03b579fe94d3bcd07f55670f246e7a
PharmacyDoc2018 <pharmacydoc2018@gmail.com>
2025-07-10 20:00:45 -0400

    updated fileserver to use the /app/ path.

--- mux.go
+++ mux.go
@@ -7,8 +7,8 @@ const port = "8080"
 
 func initMux() *http.ServeMux {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
-	mux.Handle("/assets/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/assets/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
