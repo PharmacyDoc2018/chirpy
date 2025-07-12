540ef76357f3b0805924f6b19a3410370e1b2be1
PharmacyDoc2018 <pharmacydoc2018@gmail.com>
2025-07-11 20:17:49 -0400

    fixed middleware to count server hits. added /reset path to reset the hit counter.

--- mux.go
+++ mux.go
@@ -14,8 +14,10 @@ type apiConfig struct {
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	cfg.fileserverHits.Add(1)
-	return next
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
 
 func initapiConfig() *apiConfig {
@@ -41,7 +43,13 @@ func initMux(cfg *apiConfig) *http.ServeMux {
 		numHits := cfg.fileserverHits.Load()
 		printedHits := fmt.Sprintf("Hits: %d", numHits)
 		w.Write([]byte(printedHits))
+	})
 
+	mux.HandleFunc("/reset", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		cfg.fileserverHits.Store(0)
+		w.Write([]byte("hit count reset!"))
 	})
 
 	return mux
