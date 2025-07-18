c254e1b5ea221a89f5a89f264610f1af633df809
PharmacyDoc2018 <pharmacydoc2018@gmail.com>
2025-07-18 14:34:21 -0400

    added GET /api/chirps/{id} handler that returns the chirp with the corresponding id.

--- chirps.go
+++ chirps.go
@@ -71,6 +71,7 @@ func handleResourseChirps(mux *http.ServeMux, cfg *apiConfig) {
 		if err != nil {
 			fmt.Println(err)
 			w.WriteHeader(500)
+			return
 		}
 
 		var returnedChirps []chirpResponse
@@ -89,9 +90,49 @@ func handleResourseChirps(mux *http.ServeMux, cfg *apiConfig) {
 		if err != nil {
 			fmt.Println(err)
 			w.WriteHeader(500)
+			return
 		}
 
 		w.WriteHeader(200)
 		w.Write(data)
 	})
+
+	mux.HandleFunc("GET /api/chirps/{id}", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+
+		id, err := uuid.Parse(req.PathValue("id"))
+		if err != nil {
+			fmt.Println(err)
+			w.WriteHeader(400)
+			return
+		}
+		storedChirp, err := cfg.db.GetChirp(req.Context(), id)
+		if err != nil {
+			if fmt.Sprint(err) == "sql: no rows in result set" {
+				w.WriteHeader(404)
+				return
+			} else {
+				fmt.Println(err)
+				w.WriteHeader(500)
+				return
+			}
+		}
+
+		data, err := json.Marshal(chirpResponse{
+			Id:        storedChirp.ID,
+			CreatedAt: storedChirp.CreatedAt,
+			UpdatedAt: storedChirp.UpdatedAt,
+			Body:      storedChirp.Body,
+			UserId:    storedChirp.UserID,
+		})
+		if err != nil {
+			fmt.Println(err)
+			w.WriteHeader(500)
+			return
+		}
+
+		w.WriteHeader(200)
+		w.Write(data)
+	})
+
 }
